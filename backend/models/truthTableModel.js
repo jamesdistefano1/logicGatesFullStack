143cd9ce62a2768edd6284fe0fa8aac13db913a8
mattig3090 <36996707+mattig3090@users.noreply.github.com>
2020-05-27 02:28:50 -0400

    added two properties to truth table stuff

--- backend/models/truthTableModel.js
+++ backend/models/truthTableModel.js
@@ -4,7 +4,9 @@ import truthTable from '../logicGates/src/truthTable.js'
 
 const truthTable = new Schema({
     studentTable: {type: [Number], required: true},
-    teacherTable: {type: [Number], required: true}
+    teacherTable: {type: [Number], required: true},
+    assignment: {type: Number, required: true},
+    student: {type: String, required: true}
 });
 
 module.exports = mongoose.model('TruthTable', truthTable);
--- logicGates/src/truthTable.js
+++ logicGates/src/truthTable.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 class truthTable extends React.Component{
-    constructor(studentVals, teacherVals){
+    constructor(studentVals, teacherVals, assignment, student){
         super();
         this.studentVals = studentVals;
         this.teacherVals = teacherVals;
+        this.assignment = assignment; // represents what assignment this truth table is associated with
+        this.student = student; // represents what student this is associated with 
+        
     }
 }
