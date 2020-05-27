962f4b4c951fa48c652b46294e5826cb58845c8f
Stephen Moss <smoss@udel.edu>
2020-05-26 22:56:13 -0400

    setup

--- logicGates/src/modules/application/canvas/Canvas.js
+++ logicGates/src/modules/application/canvas/Canvas.js
@@ -6,16 +6,47 @@ import Or from  './Or.js';
 import Nand from './Nand.js';
 import Nor from './Nor.js';
 
-function Canvas(){
 
-  return(
-    <div id="canvas" style={{height: '500px', width: '90%', position: 'relative', overflow: 'auto',  padding: '0'}}>
-      <And />
-      <Or />
-      <Nand />
-      <Nor />
-    </div>
-  );
+
+class Canvas extends React.Component{
+  
+  state = {
+    gateArray : [
+      {type: "OR"}, 
+      {type: "OR"},
+      {type: "AND"}
+    ]
+  }
+
+  
+
+  render(){
+
+    return(
+      <div>    
+        <div id="canvas" style={{height: '500px', width: '90%', position: 'relative', overflow: 'auto',  padding: '0'}}>
+          {
+            this.state.gateArray.map((gate)=>{
+              console.log(gate.type);
+              if (gate.type == "OR"){ 
+                return(
+                  <Or />
+	        )
+	      }
+	      if (gate.type == "AND"){
+		return (
+                  <And />
+                )
+	      }
+	    })	    
+	  }
+
+        </div>
+	<button>Spawn</button>
+      </div>
+    );
+  }
+
+
 }
-
 export default Canvas;
