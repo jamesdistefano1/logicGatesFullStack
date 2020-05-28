85244f3ecc0a25e171c01fb4a95f951585988a9e
mattig3090 <36996707+mattig3090@users.noreply.github.com>
2020-05-27 21:39:18 -0400

    clean it up a little

--- logicGates/src/truthTable.js
+++ logicGates/src/truthTable.js
@@ -6,7 +6,6 @@ class truthTable extends React.Component{
         this.studentVals = studentVals;
         this.teacherVals = teacherVals;
         this.assignment = assignment; // represents what assignment this truth table is associated with
-        this.student = student; // represents what student this is associated with 
-        
+        this.student = student; // represents what student this is associated with
     }
 }
